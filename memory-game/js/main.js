12260389d6dd7ccc7805e9d8129232258bfd971e
dfrymire79 <59580606+dfrymire79@users.noreply.github.com>
2020-01-24 05:58:49 -0600

    Add files via upload
    created game logic

---
+++ memory-game/js/main.js
@@ -0,0 +1,21 @@
+
+let cards = ["queen", "queen", "king", "king"];
+
+let cardsInPlay = [];
+
+let cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+let cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+
+} if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
+
