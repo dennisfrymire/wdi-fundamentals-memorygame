18a5b79c2d1433c5910ff1a31cbdebae26dc6901
dfrymire79 <59580606+dfrymire79@users.noreply.github.com>
2020-02-01 06:16:10 -0600

    Add files via upload
    created game logic

--- fundamentals_2/memory-game/js/main.js
+++ fundamentals_2/memory-game/js/main.js
@@ -1,21 +1,31 @@
 
-let cards = ["queen", "queen", "king", "king"];
+const cards = ["queen", "queen", "king", "king"];
 
-let cardsInPlay = [];
+const cardsInPlay = [];
 
-let cardOne = cards[0];
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("You found a match!")
+	} else {
+	console.log("Sorry, try again.");
+	  }
+	};
 
-cardsInPlay.push(cardOne);
 
-let cardTwo = cards[2];
+function flipCard(cardId) {
+	
+console.log("User flipped " + cards[cardId]);
 
-cardsInPlay.push(cardTwo);
+cardsInPlay.push(cardId);
 
-if (cardsInPlay.length === 2) {
+if (cardsInPlay.length === 2){
 
-} if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
+checkForMatch();
 }
 
+
+
+	};
+
+flipCard(0);
+flipCard(2);
