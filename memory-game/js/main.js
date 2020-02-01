6f56ba91f52ac73b80a5f67f2b53adfb4409948f
dfrymire79 <59580606+dfrymire79@users.noreply.github.com>
2020-02-01 09:39:05 -0600

    Add files via upload
    Created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,21 +1,56 @@
 
-let cards = ["queen", "queen", "king", "king"];
+const cards = [
 
-let cardsInPlay = [];
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+}
+];
 
-let cardOne = cards[0];
+const cardsInPlay = [];
 
-cardsInPlay.push(cardOne);
-
-let cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2) {
-
-} if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-} else {
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!")
+	} else {
 	alert("Sorry, try again.");
+	  }
+	};
+
+
+function flipCard(cardId) {
+	
+console.log("User flipped " + cards[cardId].rank);
+
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
+
+cardsInPlay.push(cards[cardId].rank);
+
+if (cardsInPlay.length === 2){
+
+checkForMatch();
 }
 
+
+
+	};
+
+flipCard(0);
+flipCard(2);
